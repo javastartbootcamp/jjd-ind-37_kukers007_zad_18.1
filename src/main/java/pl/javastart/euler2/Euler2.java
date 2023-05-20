286095fddc80158e8047eeb3a96c96db3969d420
Łukasz Matczuk <lukasz.matczuk84@gmail.com>
2023-05-20 13:46:08 +0200

    Solution 18.1

--- src/main/java/pl/javastart/euler2/Euler2.java
+++ src/main/java/pl/javastart/euler2/Euler2.java
@@ -1,8 +1,10 @@
 package pl.javastart.euler2;
 
 public class Euler2 {
-
     public static void main(String[] args) {
-
+        Fibonacci fibonacci = new Fibonacci();
+        int limit = 4_000_000;
+        long sumEvenFibonacci = fibonacci.sumOfEvenFibonacci(limit);
+        System.out.println("Sum of even Fibonacci numbers below " + limit + ": " + sumEvenFibonacci);
     }
 }
